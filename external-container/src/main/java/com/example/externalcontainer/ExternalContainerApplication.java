050fdc9caeafd0b0c0e0f9613f536bac0b233420
Sharad <sharad.rajore@gmail.com>
2020-11-20 17:32:16 +0530

    Added support for spring profiles

---
+++ external-container/src/main/java/com/example/externalcontainer/ExternalContainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalContainerApplication.class, args);
+	}
+
+}
--- spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
+++ spring-starter-project/src/main/java/com/zensar/SpringStarterProjectApplication.java
@@ -2,17 +2,27 @@ package com.zensar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.zensar.controllers.StudentController;
 import com.zensar.entites.Student;
 
 @SpringBootApplication
-public class SpringStarterProjectApplication  {
+public class SpringStarterProjectApplication extends SpringBootServletInitializer {
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return super.configure(builder);
+	}
+
 	public static void main(String[] args) {
 		
 		 SpringApplication.run(SpringStarterProjectApplication.class, args);
 	}
+	
+	
 	
 }
